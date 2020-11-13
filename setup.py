5b8324a8e5138397e15fc3ea7a1e6b330e6c8314
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-11-13 17:30:32 +0900

    initial release

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as r:
+  long_description=r.read()
+URL="https://github.com/KoichiYasuoka/spaCy-Alpino"
+
+setuptools.setup(
+  name="spacy_syncha",
+  version="0.1.0",
+  description="Alpino wrapper for spaCy",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url=URL,
+  author="Koichi Yasuoka",
+  author_email="yasuoka@kanji.zinbun.kyoto-u.ac.jp",
+  license="MIT",
+  keywords="spacy nlp",
+  packages=setuptools.find_packages(),
+  install_requires=["spacy>=2.2.2","deplacy>=1.8.1"],
+  python_requires=">=3.6",
+  package_data={"spacy_syncha":["./alpino2ud.sh","./universal_dependencies.xq"]},
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "Topic :: Text Processing :: Linguistic",
+    "Natural Language :: Dutch"
+  ],
+  project_urls={
+    "Alpino":"http://www.let.rug.nl/vannoord/alp/Alpino/",
+    "Source":URL,
+    "Tracker":URL+"/issues"
+  }
+)
