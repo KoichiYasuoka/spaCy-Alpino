427b8e499edbf5bef345847eec9e5ef93e54773b
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-11-23 18:53:07 +0900

    spaCy v3.0 support

--- spacy_alpino/alpino.py
+++ spacy_alpino/alpino.py
@@ -9,6 +9,8 @@ ALPINO_URL="https://urd2.let.rug.nl/~vannoord/bin/"
 import numpy
 from spacy.symbols import LEMMA,POS,TAG,DEP,HEAD
 from spacy.tokens import Doc
+from spacy.language import Language
+SPACY_V3=hasattr(Language,"component")
 
 class AlpinoParser(object):
   name="Alpino"
@@ -42,6 +44,7 @@ class AlpinoParser(object):
     lemmas=[]
     pos=[]
     tags=[]
+    morphs=[]
     heads=[]
     deps=[]
     for t in u.split("\n"): 
@@ -50,10 +53,11 @@ class AlpinoParser(object):
       s=t.split("\t")
       if len(s)!=10:
         continue
-      id,_,lemma,upos,xpos,_,head,deprel,_,_=s
+      id,_,lemma,upos,xpos,feats,head,deprel,_,_=s
       lemmas.append(vs.add(lemma))
       pos.append(vs.add(upos))
       tags.append(vs.add(xpos))
+      morphs.append(feats)
       if deprel=="root" or deprel=="ROOT":
         heads.append(0)
         deps.append(r)
@@ -65,15 +69,25 @@ class AlpinoParser(object):
         deps.append(vs.add(deprel))
     a=numpy.array(list(zip(lemmas,pos,tags,deps,heads)),dtype="uint64")
     doc.from_array([LEMMA,POS,TAG,DEP,HEAD],a)
-    doc.is_tagged=True
-    doc.is_parsed=True
+    if SPACY_V3:
+      for i,j in enumerate(morphs):
+        if j!="_" and j!="":
+          doc[i].set_morph(j)
+    else:
+      doc.is_tagged=True
+      doc.is_parsed=True
     return doc
 
 def load(command="Alpino"):
   from spacy.lang.nl import Dutch
   nlp=Dutch()
-  nlp.add_pipe(nlp.create_pipe("sentencizer"))
-  nlp.add_pipe(AlpinoParser(command,nlp.vocab))
+  if SPACY_V3:
+    nlp.add_pipe("sentencizer")
+    Language.component("Alpino",func=AlpinoParser(command,nlp.vocab))
+    nlp.add_pipe("Alpino")
+  else:
+    nlp.add_pipe(nlp.create_pipe("sentencizer"))
+    nlp.add_pipe(AlpinoParser(command,nlp.vocab))
   nlp.meta["lang"]="nl_Alpino"
   return nlp
 
