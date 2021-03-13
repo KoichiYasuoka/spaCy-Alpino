8d283609c40d27f6c9108ea2c441e5b9c50668d1
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-03-13 10:39:51 +0900

    version 0.7.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-Alpino"
 
 setuptools.setup(
   name="spacy_alpino",
-  version="0.7.1",
+  version="0.7.2",
   description="Alpino wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
   license="LGPL",
   keywords="spacy nlp",
   packages=setuptools.find_packages(),
-  install_requires=["spacy>=2.2.2","deplacy>=1.8.8"],
+  install_requires=["spacy>=2.2.2","deplacy>=1.9.2"],
   python_requires=">=3.6",
   package_data={"spacy_alpino":["./alpino2ud.sh","./universal_dependencies.xq"]},
   classifiers=[
