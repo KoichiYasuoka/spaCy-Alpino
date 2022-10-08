698fda7bf8918ca4f939f20c1392b1b2de11b146
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-10-09 00:53:01 +0900

    patch for Alpino 2022-09-19

--- spacy_alpino/alpino2ud.sh
+++ spacy_alpino/alpino2ud.sh
@@ -3,7 +3,7 @@ TMP=/tmp/alpino$$
 trap "/bin/rm -fr $TMP ; exit 2" 1 2 3 15
 mkdir $TMP
 
-( ${1-Alpino} -flag treebank $TMP end_hook=xml -parse
+( ${1-Alpino} -flag treebank $TMP end_hook=xml -parse > /dev/null
   ( echo '<collection>'
     ls -v1 $TMP/[1-9]*.xml | sed 's?.*?<doc href="&" />?'
     echo '</collection>'
